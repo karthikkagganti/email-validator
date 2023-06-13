a01a65494d4ea44c1b9badbe38faa8640321d6fb
karthikkagganti <karthikkagganti@gmail.com>
2023-06-13 12:52:50 +0530

    Updated condition for Toast

--- src/app/Login/page.js
+++ src/app/Login/page.js
@@ -22,7 +22,7 @@ export default function Login() {
               if (!email) {
                 return toast.error('Please fill all the fields');
               }
-              validateEmail(email?.split('@')[1]) ? toast.error('Please Enter work email') : ()=>{sendMagicLink(email, null); toast.success(`Please login using the link sent to ${email}`);};
+              validateEmail(email?.split('@')[1]) ? toast.error('Please Enter work email') : sendMagicLink(email, null); toast.success(`Please login using the link sent to ${email}`);
 //               sendMagicLink(email, null);
               
             }}
