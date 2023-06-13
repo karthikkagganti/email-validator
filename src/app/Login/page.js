4f1158a2d2167f23317171f01752827671e97667
karthikkagganti <56468469+karthikkagganti@users.noreply.github.com>
2023-06-13 12:43:12 +0530

    Update page.js

--- src/app/Login/page.js
+++ src/app/Login/page.js
@@ -22,9 +22,9 @@ export default function Login() {
               if (!email) {
                 return toast.error('Please fill all the fields');
               }
-              validateEmail(email?.split('@')[1]) ? toast.error('Please Enter work email') : sendMagicLink(email, null);
+              validateEmail(email?.split('@')[1]) ? toast.error('Please Enter work email') : ()=>{sendMagicLink(email, null); toast.success(`Please login using the link sent to ${email}`);};
 //               sendMagicLink(email, null);
-              toast.success(`Please login using the link sent to ${email}`)
+              
             }}
           >
             <div className="">
